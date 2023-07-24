26529820340beeaf1d8e1ec8544dfe01f6954a23
Jenish <jenish.rutilant@gmail.com>
2023-07-24 16:25:47 +0530

    okk

--- Admin/Middleware/Admin_Middleware.js
+++ Admin/Middleware/Admin_Middleware.js
@@ -1,22 +1,21 @@
-var jwt = require('jsonwebtoken');
-require('dotenv').config()
+var jwt = require("jsonwebtoken");
+require("dotenv").config();
 const JWT_SECRET = process.env.JWT_SECRET;
 
 const fetchadmin = (req, res, next) => {
-    // Get the vendor from the jwt token and add id to req object
-    const token = req.header('authToken_admin');
-    if (!token) {
-        res.status(401).send({ error: "Please authenticate using a valid token " })
-    }
-    // console.log(data.admin)
-    try {
-        const data = jwt.verify(token, JWT_SECRET);
-        req.admin = data.admin;
-        next();
-    } catch (err) {
-        res.status(401).send({ error: "User does not exist" })
-    }
-
-}
+  // Get the vendor from the jwt token and add id to req object
+  const token = req.header("authToken_admin");
+  if (!token) {
+    res.status(401).send({ error: "Please authenticate using a valid token " });
+  }
+  // console.log(data.admin)
+  try {
+    const data = jwt.verify(token, JWT_SECRET);
+    req.admin = data.admin;
+    next();
+  } catch (err) {
+    res.status(401).send({ error: "User does not exist" });
+  }
+};
 
 module.exports = fetchadmin;
